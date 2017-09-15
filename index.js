16d2c9920d60c1a7810e46c3283c587559c9b10f
munteanualex <31924989+munteanualex@users.noreply.github.com>
2017-09-15 19:00:14 +0300

    last 15.09

--- index.js
+++ index.js
@@ -1,19 +1,54 @@
-var blessed = require('blessed')
-  , screen = blessed.screen();
+var blessed = require('blessed');
 
-var form = blessed.form({
-  parent: screen,
+// Create a screen object.
+var screen = blessed.screen();
+
+// Create a box perfectly centered horizontally and vertically.
+var box = blessed.box({
+  top: 'center',
+  left: 'center',
+  width: '80%',
+  height: '100%',
+  content: 'Car Manager (v1.0.0)',
+  align: 'center',
+  tags: true,
+  border: {
+    type: 'line'
+  },
+  style: {
+    fg: 'white',
+    bg: '#ADD8E6',
+    border: {
+      fg: '#ffffff'
+    },
+    hover: {
+      bg: 'green'
+    }
+  }
+});
+var form1 = blessed.form({
+  parent: box,
   keys: true,
-  left: 0,
-  top: 0,
+  left: 5,
+  top: 5,
   width: 30,
   height: 4,
-  bg: 'green',
-  content: 'Submit or cancel?'
+  bg: '#ADD8E6',
+  content: 'Add a new car'
+});
+var form2 = blessed.form({
+  parent: box,
+  keys: true,
+  left: 5,
+  top: 10,
+  width: 30,
+  height: 4,
+  bg: '#ADD8E6',
+  content: 'Delete old car'
 });
 
-var submit = blessed.button({
-  parent: form,
+var addCar = blessed.button({
+  parent: form1,
   mouse: true,
   keys: true,
   shrink: true,
@@ -21,11 +56,11 @@ var submit = blessed.button({
     left: 1,
     right: 1
   },
-  left: 10,
+  left: 3,
   top: 2,
   shrink: true,
-  name: 'submit',
-  content: 'submit',
+  name: 'Add Car',
+  content: 'Add Car',
   style: {
     bg: 'blue',
     focus: {
@@ -37,8 +72,8 @@ var submit = blessed.button({
   }
 });
 
-var cancel = blessed.button({
-  parent: form,
+var delCar = blessed.button({
+  parent: form2,
   mouse: true,
   keys: true,
   shrink: true,
@@ -46,11 +81,11 @@ var cancel = blessed.button({
     left: 1,
     right: 1
   },
-  left: 20,
+  left: 3,
   top: 2,
   shrink: true,
-  name: 'cancel',
-  content: 'cancel',
+  name: 'Delete car',
+  content: 'Delete car',
   style: {
     bg: 'blue',
     focus: {
@@ -62,21 +97,21 @@ var cancel = blessed.button({
   }
 });
 
-submit.on('press', function() {
-  form.submit();
+addCar.on('press', function() {
+  form1.submit();
 });
 
-cancel.on('press', function() {
-  form.reset();
+delCar.on('press', function() {
+  form2.reset();
 });
 
-form.on('submit', function(data) {
-  form.setContent('Submitted.');
+form1.on('Add new car', function(data) {
+  form1.setContent('Submitted.');
   screen.render();
 });
 
-form.on('reset', function(data) {
-  form.setContent('Canceled.');
+form2.on('Delete old car', function(data) {
+  form.setContent('Deleted old car.');
   screen.render();
 });
 
@@ -84,4 +119,33 @@ screen.key('q', function() {
   process.exit(0);
 });
 
+screen.render();
+
+
+// Append our box to the screen.
+screen.append(box);
+
+// If our box is clicked, change the content.
+box.on('click', function(data) {
+  box.setContent('{center}Some different {red-fg}content{/red-fg}.{/center}');
+  screen.render();
+});
+
+// If box is focused, handle `enter` and give us some more content.
+box.key('enter', function() {
+  box.setContent('{right}Even different {black-fg}content{/black-fg}.{/right}\n');
+  box.setLine(1, 'bar');
+  box.insertLine(1, 'foo');
+  screen.render();
+});
+
+// Quit on Escape, q, or Control-C.
+screen.key(['escape', 'q', 'C-c'], function(ch, key) {
+  return process.exit(0);
+});
+
+// Focus our element.
+box.focus();
+
+// Render the screen.
 screen.render();
